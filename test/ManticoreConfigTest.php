dd882bd4e57bc3c251f7cce549ef3532f0cd6686
Don Hardman <don.hardman@muvon.io>
2022-10-11 16:16:53 +0700

    Add license header to all files

--- test/ManticoreConfigTest.php
+++ test/ManticoreConfigTest.php
@@ -1,5 +1,14 @@
 <?php declare(strict_types=1);
 
+/*
+  Copyright (c) 2022, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
 use PHPUnit\Framework\TestCase;
 
 class ManticoreConfigTest extends TestCase {
