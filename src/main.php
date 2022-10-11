9967906d035bb61e59be43361e4a88fb6989e8b1
Sergey Nikolaev <prostuda@academ.org>
2022-10-11 15:49:45 +0700

    License-related changes

--- src/main.php
+++ src/main.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
 
+// Copyright (c) 2022, Manticore Software LTD (https://manticoresearch.com)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 or any later
+// version. You should have received a copy of the GPL license along with this
+// program; if you did not, you can find it at http://www.gnu.org/
+
 /**
  * 0. Check dependencies (? still no deps and thats good) and minimal PHP versions
  * 1. Parse the arguments
