6e61fa7700fbcc42184476d34d253f3767f1d290
Don Hardman <don.hardman@muvon.io>
2022-10-19 13:28:29 +0200

    Update version to 0.2.25

--- src/Lib/ManticoreBackup.php
+++ src/Lib/ManticoreBackup.php
@@ -18,7 +18,7 @@ use function println;
  * This class is used to initialize config, parse it and launch the backup process
  */
 class ManticoreBackup {
-	const VERSION = '0.2.23';
+	const VERSION = '0.2.25';
 
   /**
    * Store the wanted tables in backup dir as backup
