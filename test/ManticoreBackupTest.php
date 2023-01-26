aff9ffe92d5e6006aa4dde30e83f1413855b98a9
Don Hardman <don.hardman@muvon.io>
2023-01-26 04:40:30 +0000

    Update copyrights

--- test/ManticoreBackupTest.php
+++ test/ManticoreBackupTest.php
@@ -7,7 +7,7 @@ use Manticoresearch\Backup\Lib\ManticoreClient;
 use Manticoresearch\Backup\Lib\ManticoreConfig;
 
 /*
-  Copyright (c) 2022, Manticore Software LTD (https://manticoresearch.com)
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License version 2 or any later
